b7232a674b726ba295c3dbee97640f4edbff1c5a
FelipeArismendi <farismendi27@gmail.com>
2022-06-02 20:40:01 -0500

    seccion Inicio finalizada

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const menuBars = document.getElementById('menu__bars')
+const menuNav = document.getElementById('menu__nav')
+const menuBack = document.getElementById('menu__back')
+const menuItems = document.querySelectorAll('.menu__item')
+const secciones = document.querySelectorAll('.seccion')
+
+// menu nav interactions start
+menuBars.addEventListener('click', () => {
+  menuNav.classList.toggle('active')
+  menuBack.classList.toggle('show')
+})
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    menuNav.classList.remove('active')
+    menuBack.classList.remove('show')
+  })
+})
+
+// menu nav interactions end
+
+// observer start
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry.target.id)
+        const id = entry.target.id
+        menuItems.forEach((item) => {
+          if (item.classList.contains(id)) {
+            item.classList.add('selected')
+          } else {
+            item.classList.remove('selected')
+          }
+        })
+      }
+    })
+  },
+  {
+    rootMargin: '-80px 0px 0px 0px',
+    threshold: 0.5,
+  },
+)
+
+// asignacion de observador
+secciones.forEach((seccion) => observer.observe(seccion))
+
+// observer end
